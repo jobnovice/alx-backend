19581874e87d55c100667d072bd397d25a3415b2
jobnovice <jobkebede47@gmail.com>
2024-10-31 23:10:29 +0300

    Basics

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Basic dictionary"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """new class that inherits from BaseCaching"""
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        self.cache_data[f"{key}"] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data[key] if key in self.cache_data.keys() else None

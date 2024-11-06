975d30cda073f786660e157f612aad5d2a3827a1
jobnovice <jobkebede47@gmail.com>
2024-11-06 18:29:27 +0300

    Prefferd languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""2-app.py"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """wrote a class that configures the flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """returns the preffered language"""
+    return request.accept_languages.bestmatch(["en", "fr"])
+
+@app.route('/')
+def home():
+    """defines the route the landing page"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
